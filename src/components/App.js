eff6d33cf7957937e45b323b9132d49095f80894
iratxetos <iratxetxu@gmail.com>
2021-09-27 01:25:34 +0200

    added new features

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 function App() {
   const [data, setData] = useState(initialData);
-
+  const [searchClub, setSearch] = useState('all');
   const [newClub, setNewClub] = useState(
     {
       name: '',
@@ -13,20 +13,10 @@ function App() {
     }
   );
 
-  const htmlClubList = data.map((oneClub, index) => (
-    <li className='club__item' key={index}>
-      <p className='club__name'>
-        {oneClub.name}
-      </p>
-      <p className='club__weekdays'>
-        {`Abierto entre semana: ${oneClub.openOnWeekdays}`}
-      </p>
-      <p className='club__weekend'>
-        {`Abierto el fin de semana: ${oneClub.openOnWeekend}`}
-      </p>
+  const handleSearchClub = (ev) => {
+    setSearch(ev.target.value);
 
-    </li>
-  ));
+  };
 
   const handleChangeNewClub = (ev) => {
     const elementIdChanged = (ev.currentTarget.id);
@@ -47,16 +37,63 @@ function App() {
     });
   };
 
+  const handleDeleteClub = (ev) => {
+    const deletedClub = ev.currenTarget.id;
+    const newData = data.splice(deletedClub, 1);
+    setData([...newData]);
+
+  };
+
+  const renderClubList = () => {
+    return data
+      .filter((data) => {
+        if (searchClub === 'openOnWeekDays') {
+          return data.openOnWeekdays === true;
+        } else if (searchClub === 'openOnWeekend') {
+          return data.openOnWeekend === true;
+        }
+        return true;
+      })
+
+      .map((club, index) => (
+        <li className='club__item' key={index}>
+          <div className='club__section' onClick={handleDeleteClub}>
+            <p className='club__name'>
+              #{index} {club.name}
+            </p>
+            <p className='club__weekdays'>
+              {`Abierto entre semana: ${club.openOnWeekdays ? 'Si' : 'No'}`}
+            </p>
+            <p className='club__weekend'>
+              {`Abierto el fin de semana: ${club.openOnWeekend ? 'Si' : 'No'}`}
+            </p>
+          </div>
+          <div className='club__delete'>
+            <p className='club__delete--cross' onClick={handleDeleteClub}>X</p>
+          </div>
+        </li>
+      ));
+  };
+
+
+
   return (
     <div className='page'>
       {/* header */}
       <header className='header'>
         <h1 className='header__title'>Mis clubs</h1>
+        <form action="">
+          <select onChange={handleSearchClub}>
+            <option value="all">Todos</option>
+            <option value="openOnWeekdays">Los que abren entre semana</option>
+            <option value="openOnWeekend">Lo que abren fines de semana</option>
+          </select>
+        </form>
       </header>
 
       <main>
         {/* clubs list */}
-        <ul className='club__list'>{htmlClubList}</ul>
+        <ul className='club__list'>{renderClubList()}</ul>
 
         {/* new club */}
         <form className='new-club__form'>
@@ -64,7 +101,7 @@ function App() {
           <label className='new-club__label'>
             Nombre del club
             <input
-              className='new-club__input'
+              className='new-club__label--input'
               type='text'
               name='name'
               id='name'
@@ -75,22 +112,22 @@ function App() {
           </label>
           <label className='new-club__label'> ¿Abre entre semana?
             <input
-              className='new-club__input'
+              className='new-club__label--input'
               type='checkbox'
               name='openOnWeekdays'
               id='openOnWeekdays'
               onChange={handleChangeNewClub}
-              value={newClub.openOnWeekdays = true}
+              checked={newClub.openOnWeekdays}
             />
           </label>
           <label className='new-club__label'>¿Abre los fines de semana?
             <input
-              className='new-club__input'
+              className='new-club__label--input'
               type='checkbox'
               name='openOnWeekend'
               id='openOnWeekend'
               onChange={handleChangeNewClub}
-              value={newClub.openOnWeekend = true}
+              checked={newClub.openOnWeekend}
             />
           </label>
           <input
@@ -103,6 +140,7 @@ function App() {
       </main>
     </div>
   );
+
 }
 
 export default App;
